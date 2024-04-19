838e9c40a243992c0e3a523a30aa421964aaa0d4
GabrieleIncardona <gabriele.incardona@studenti.unipr.it>
2024-04-19 11:29:12 +0200

    update main

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from netsquid_netbuilder.run import get_default_builder
-from squidasm.sim.stack.context import NetSquidContext
-from squidasm.sim.stack.stack import ProcessingNode, NodeStack
-import itertools
-
 from struttura import create_network
 import traceback
 from router import *
@@ -31,28 +26,6 @@ def main():
     for node_name in node_names:
         router = Router(node_name, links, TeleportParams.generate_random_params())
         programs[node_name] = router
-
-    NetSquidContext.reset()
-    builder = get_default_builder()
-    network = builder.build(network_config)
-    stacks = {}
-    for node_name, node in network.nodes.items():
-        assert isinstance(node, ProcessingNode)
-        stack = NodeStack(name=node_name, node=node, qdevice_type=node.qmemory_typ)
-        NetSquidContext.add_node(stack.node.ID, node_name)
-        stacks[node_name] = stack
-
-    for id_tuple, egp in network.egp.items():
-        node_name, peer_name = id_tuple
-        stacks[node_name].assign_egp(network.node_name_id_mapping[peer_name], egp)
-
-    for s1, s2 in itertools.combinations(stacks.values(), 2):
-        print(s1.name, s2.name)
-        print(s1.qnos.netstack._comp.ports)
-        s1.qnos.netstack.register_peer(s2.node.ID)
-        s1.qnos_comp.register_peer(s2.node.ID)
-        s2.qnos.netstack.register_peer(s1.node.ID)
-        s2.qnos_comp.register_peer(s1.node.ID)
 
     run(config=network_config, programs=programs, num_times=1)
 
