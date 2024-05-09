52f79d950f78f82e97be17a331e44895e6aec3b5
GabrieleIncardona <gabriele.incardona@studenti.unipr.it>
2024-05-09 10:23:07 +0200

    refactory

--- router.py
+++ router.py
@@ -131,7 +131,6 @@ class Router(Program):
                     yield from self.create_quantum_link_intermedio(queue_protocol, context)
                 else:
                     yield from self.create_quantum_link_ultimo(queue_protocol, context)
-
             elif self.msg == "Ripristina disponibilita":
                 self.ripristina_disponibilita(context)
 
