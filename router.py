623ba4b9eeb5218a6f79a45bfacd9eb9a758a206
GabrieleIncardona <gabriele.incardona@studenti.unipr.it>
2024-05-20 13:18:03 +0200

    translation of the code into English

--- router.py
+++ router.py
@@ -8,7 +8,6 @@ from netqasm.sdk import Qubit
 from netqasm.sdk.classical_communication.message import StructuredMessage
 from netqasm.sdk.toolbox.state_prep import set_qubit_state
 
-
 from squidasm.sim.stack.common import LogManager
 from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
 from squidasm.util import get_qubit_state, get_reference_state
@@ -29,23 +28,23 @@ class TeleportParams:
 
 class Router(Program):
     msg = ""
-    disponibilita = 1
-    mittente = None
-    mittente2 = None
-    iniziatore = 0
-    ultimo = 0
-    collegamento_gia_creato = 0
-    ciclo = True
-    receveid = ""
+    availability = 1
+    sender = None
+    sender2 = None
+    initiator = 0
+    last = 0
+    link_already_created = 0
+    cycle = True
+    receiver = ""
 
-    def __init__(self, insieme, link, params: TeleportParams, senders, receiveds):
+    def __init__(self, jointly, link, params: TeleportParams, senders, receivers):
         self.logger = LogManager.get_stack_logger(self.__class__.__name__)
-        self.insieme = insieme
+        self.jointly = jointly
         self.link = link
         self.phi = params.phi
         self.theta = params.theta
         self.senders = senders
-        self.receiveds = receiveds
+        self.receivers = receivers
 
     @property
     def meta(self) -> ProgramMeta:
@@ -57,230 +56,212 @@ class Router(Program):
         )
 
     def run(self, context: ProgramContext):
-        # self.disponibilita = 1
 
-        # if self.insieme == "A1":
-        #    self.send_message("Voglio comunicare con D1", context)
-
-        if self.insieme in self.senders:
-            self.receveid = random.choice(self.receiveds)
-            if self.insieme != self.receveid:
-                self.send_message(f"Voglio comunicare con {self.receveid}", context)
-                print(f"{self.insieme} would like to communicate with {self.receveid}")
+        if self.jointly in self.senders:
+            self.receiver = random.choice(self.receivers)
+            if self.jointly != self.receiver:
+                self.send_message(f"I want to communicate with {self.receiver}", context)
+                print(f"{self.jointly} would like to communicate with {self.receiver}")
 
         yield from self.receive_message(context)
 
     def send_message(self, msg, context):
-        if self.receveid in self.link:
-            csocket1 = context.csockets[self.receveid]
+        if self.receiver in self.link:
+            csocket1 = context.csockets[self.receiver]
             csocket1.send("direct connection")
-            self.iniziatore = 1
+            self.initiator = 1
             self.msg = msg
-            self.disponibilita = 0
+            self.availability = 0
             return
         else:
             csocket = []
             for i in range(len(self.link)):
                 csocket.append(context.csockets[self.link[i]])
-            self.iniziatore = 1
+            self.initiator = 1
             self.msg = msg
-            self.disponibilita = 0
+            self.availability = 0
             # yield from connection.flush()
             for i in range(len(self.link)):
                 csocket[i].send(msg)
 
     def receive_message(self, context):
-        # if self.insieme == "B1":
-        #    print(f"{self.insieme:} {self.link}")
         queue_protocol = QueueProtocol()
         queue_protocol.start()
 
-        collegamento = []
+        connection = []
 
         for i in range(len(self.link)):
             listener = (
                 CSocketListener(context, self.link[i], queue_protocol, self.logger))
             listener.start()
-        while self.ciclo:
+        while self.cycle:
             client_name, msg = yield from queue_protocol.pop()
-            # print(client_name)
 
             for i in range(len(self.link)):
                 if self.link[i] != client_name:
-                    collegamento.append(self.link[i])
-                    #print(f"{self.insieme} {self.link[i]}")
+                    connection.append(self.link[i])
 
             self.msg = msg
-            # print(f"{self.insieme} ho ricevuto {self.msg} ")
-            if self.msg.startswith("Voglio comunicare con "):
-                self.mittente = client_name
-                sottomessaggio = self.msg.split("Voglio comunicare con ")
-                if self.insieme == sottomessaggio[1]:
-                    if self.disponibilita == 1:
-                        self.disponibilita = 0
-                        self.ultimo = 1
-                        self.send_risposta(context)
+            if self.msg.startswith("I want to communicate with "):
+                self.sender = client_name
+                submission = self.msg.split("I want to communicate with ")
+                if self.jointly == submission[1]:
+                    if self.availability == 1:
+                        self.availability = 0
+                        self.last = 1
+                        self.send_answer(context)
                     else:
-                        self.Notsend_risposta(context)
+                        self.Notsend_answer(context)
                 else:
-                    if self.disponibilita == 1:
-                        self.chiedi_disponibilita(context, collegamento)
+                    if self.availability == 1:
+                        self.ask_availability(context, connection)
                     else:
-                        self.Notsend_risposta(context)
-            elif self.msg == "Disponibile":
-                if self.collegamento_gia_creato == 0:
-                    self.collegamento_gia_creato = 1
-                    self.mittente2 = client_name
+                        self.Notsend_answer(context)
+            elif self.msg == "Available":
+                if self.link_already_created == 0:
+                    self.link_already_created = 1
+                    self.sender2 = client_name
 
-                    # print(f"{self.insieme} disponibile")
-
-                    if self.iniziatore == 0:
-                        self.send_risposta(context)
+                    if self.initiator == 0:
+                        self.send_answer(context)
                     else:
-                        # print("io insieme:" + self.insieme + " sto creando entanglement ")
-                        self.mittente = client_name
-                        self.mittente2 = client_name
-                        self.ciclo = False
-                        yield from self.create_quantum_link_iniziale(queue_protocol, context)
+                        self.sender = client_name
+                        self.sender2 = client_name
+                        self.cycle = False
+                        yield from self.create_quantum_link_initial(queue_protocol, context)
                 else:
-                    self.ripristina_disponibilita(context)
+                    self.Restore_availability(context)
 
-            elif self.msg.startswith("Invio epr"):
-                self.mittente = client_name
-                self.ciclo = False
-                # print("io insieme:" + self.insieme + " sto creando entanglement ")
-                if self.ultimo == 0:
-                    yield from self.create_quantum_link_intermedio(queue_protocol, context)
+            elif self.msg.startswith("Sending epr"):
+                self.sender = client_name
+                self.cycle = False
+                if self.last == 0:
+                    yield from self.create_quantum_link_intermediate(queue_protocol, context)
                 else:
-                    yield from self.create_quantum_link_ultimo(queue_protocol, context)
-            elif self.msg == "Ripristina disponibilita":
-                self.ripristina_disponibilita(context)
+                    yield from self.create_quantum_link_last(queue_protocol, context)
+            elif self.msg == "Restore availability":
+                self.Restore_availability(context)
 
             elif self.msg == "direct connection":
-                self.mittente = client_name
-                self.disponibilita = 0
+                self.sender = client_name
+                self.availability = 0
                 yield from self.create_quantum_link_direct_received(queue_protocol, context)
 
             elif self.msg == "would like to communicate with":
-                self.mittente = client_name
+                self.sender = client_name
                 yield from self.create_quantum_link_direct_sender(context)
 
             self.msg = " "
 
-    def chiedi_disponibilita(self, context, collegamento):
-        # print(f"{self.insieme} {collegamento}")
-        for i in range(len(collegamento)):
-            csocket = context.csockets[collegamento[i]]
+    def ask_availability(self, context, connection):
+        for i in range(len(connection)):
+            csocket = context.csockets[connection[i]]
             csocket.send(self.msg)
 
-    def send_risposta(self, context):
-        csocket = context.csockets[self.mittente]
-        csocket.send("Disponibile")
+    def send_answer(self, context):
+        csocket = context.csockets[self.sender]
+        csocket.send("Available")
 
-    def Notsend_risposta(self, context):
-        csocket = context.csockets[self.mittente]
-        csocket.send("No disponibile")
+    def Notsend_answer(self, context):
+        csocket = context.csockets[self.sender]
+        csocket.send("No Available")
 
-    def ripristina_disponibilita(self, context):
-        csocket = context.csockets[self.mittente]
-        self.disponibilita = 1
-        csocket.send("Ripristina disponibilita")
+    def Restore_availability(self, context):
+        csocket = context.csockets[self.sender]
+        self.availability = 1
+        csocket.send("Restore availability")
 
-    def create_quantum_link_iniziale(self, queue_protocol, context):
-        csocket = context.csockets[self.mittente2]
-        epr_socket = context.epr_sockets[self.mittente2]
+    def create_quantum_link_initial(self, queue_protocol, context):
+        csocket = context.csockets[self.sender2]
+        epr_socket = context.epr_sockets[self.sender2]
         connection = context.connection
         q = Qubit(connection)
         set_qubit_state(q, self.phi, self.theta)
         yield from connection.flush()
         # Create EPR pairs
-        csocket.send("Invio epr")
+        csocket.send("Sending epr")
         epr = epr_socket.create_keep()[0]
         yield from connection.flush()
 
-        # invio segnale di buffer
-        csocket.send("sono il primo")
-        misurazioni_ottenute = []
+        # sending buffer signal
+        csocket.send("I'm the first")
+        measurements_obtained = []
         client_name, msg = yield from queue_protocol.pop()
-        # print(f"{self.insieme } ho ricevuto {msg}")
-        elementi = msg[1:-1].split(', ')
-        for i in elementi:
-            misurazioni_ottenute.append(i)
-        for i in misurazioni_ottenute:
+        elements = msg[1:-1].split(', ')
+        for i in elements:
+            measurements_obtained.append(i)
+        for i in measurements_obtained:
             if i == '1':
                 epr.Z()
         yield from connection.flush()
-        # print(f"{self.insieme}: {get_qubit_state(epr, node_name=self.insieme, full_state=True)}")
         mo = epr.measure()
         yield from connection.flush()
         mo = int(mo)
-        print(self.insieme + " ho misurato: " + mo.__str__())
+        print(self.jointly + " I measured: " + mo.__str__())
 
-    def create_quantum_link_intermedio(self, queue_protocol, context):
-        csocket1 = context.csockets[self.mittente]
-        csocket2 = context.csockets[self.mittente2]
-        epr_socket1 = context.epr_sockets[self.mittente]
-        epr_socket2 = context.epr_sockets[self.mittente2]
-        misurazioni_intermedie = []
-        misurazioni = []
+    def create_quantum_link_intermediate(self, queue_protocol, context):
+        csocket1 = context.csockets[self.sender]
+        csocket2 = context.csockets[self.sender2]
+        epr_socket1 = context.epr_sockets[self.sender]
+        epr_socket2 = context.epr_sockets[self.sender2]
+        intermediate_measurements = []
+        measurements = []
 
         connection = context.connection
         epr0 = epr_socket1.recv_keep()[0]
         yield from connection.flush()
 
-        csocket2.send("Invio epr")
+        csocket2.send("Sending epr")
         epr1 = epr_socket2.create_keep()[0]
         yield from connection.flush()
         client_name, msg = yield from queue_protocol.pop()
-        # print(f"{self.insieme} ho ricevuto {msg} da {client_name}")
-        if msg != "sono il primo":
-            elementi = msg[1:-1].split(', ')
-            for i in elementi:
-                misurazioni_intermedie.append(int(i))
+        
+        if msg != "I'm the first":
+            elements = msg[1:-1].split(', ')
+            for i in elements:
+                intermediate_measurements.append(int(i))
         epr0.cnot(epr1)
         epr0.H()
         m0 = epr0.measure()
         m1 = epr1.measure()
         yield from connection.flush()
-        misurazioni_intermedie.append(int(m1))
-        csocket2.send(misurazioni_intermedie.__str__())
+        intermediate_measurements.append(int(m1))
+        csocket2.send(intermediate_measurements.__str__())
         client_name, msg = yield from queue_protocol.pop()
-        if msg != "sono l'ultimo":
-            elementi = msg[1:-1].split(', ')
-            for i in elementi:
-                misurazioni.append(int(i))
-        misurazioni.append(int(m0))
-        csocket1.send(misurazioni.__str__())
+        if msg != "I'm last":
+            elements = msg[1:-1].split(', ')
+            for i in elements:
+                measurements.append(int(i))
+        measurements.append(int(m0))
+        csocket1.send(measurements.__str__())
 
-    def create_quantum_link_ultimo(self, queue_protocol, context):
-        csocket = context.csockets[self.mittente]
-        epr_socket = context.epr_sockets[self.mittente]
+    def create_quantum_link_last(self, queue_protocol, context):
+        csocket = context.csockets[self.sender]
+        epr_socket = context.epr_sockets[self.sender]
         connection = context.connection
         q = Qubit(connection)
         set_qubit_state(q, self.phi, self.theta)
         yield from connection.flush()
         epr = epr_socket.recv_keep()[0]
         yield from connection.flush()
-        csocket.send("sono l'ultimo")
-        misurazioni_intermedie = []
+        csocket.send("I'm last")
+        intermediate_measurements = []
         client_name, msg = yield from queue_protocol.pop()
-        elementi = msg[1:-1].split(', ')
-        # print(f"{self.insieme} ho ricevuto {msg}")
-        for i in elementi:
-            misurazioni_intermedie.append(i)
-        for i in misurazioni_intermedie:
+        elements = msg[1:-1].split(', ')
+        for i in elements:
+            intermediate_measurements.append(i)
+        for i in intermediate_measurements:
             if i == '1':
                 epr.X()
         yield from connection.flush()
-        # print(f"{self.insieme}: {get_qubit_state(epr, node_name=self.insieme, full_state=True)}")
         mo = epr.measure()
         yield from connection.flush()
         mo = int(mo)
-        print(self.insieme + " ho misurato: " + mo.__str__())
+        print(self.jointly + " I measured: " + mo.__str__())
 
     def create_quantum_link_direct_sender(self, context):
-        csocket = context.csockets[self.mittente]
-        epr_socket = context.epr_sockets[self.mittente]
+        csocket = context.csockets[self.sender]
+        epr_socket = context.epr_sockets[self.sender]
         connection = context.connection
 
         q = Qubit(connection)
@@ -297,11 +278,11 @@ class Router(Program):
 
         csocket.send(m1)
 
-        print(self.insieme + " ho misurato: " + m1.__str__())
+        print(self.jointly + " I measured: " + m1.__str__())
 
     def create_quantum_link_direct_received(self, queue_protocol, context):
-        csocket = context.csockets[self.mittente]
-        epr_socket = context.epr_sockets[self.mittente]
+        csocket = context.csockets[self.sender]
+        epr_socket = context.epr_sockets[self.sender]
         connection = context.connection
         csocket.send("would like to communicate with")
 
@@ -314,11 +295,11 @@ class Router(Program):
             if msg.isdigit():
                 break
             csocket1 = context.csockets[client_name]
-            csocket1.send("No disponibile")
+            csocket1.send("No Available")
         m1 = int(msg)
         if int(m1) == 1:
             epr.Z()
         m2 = epr.measure()
         yield from connection.flush()
         m2 = int(m2)
-        print(self.insieme + " ho misurato: " + m2.__str__())
+        print(self.jointly + " I measured: " + m2.__str__())
